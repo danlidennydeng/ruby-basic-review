f44bb7270f0241be941bd882dd94371a8795fe61
Danli Denny Deng <danlideng@yahoo.com>
2021-10-12 05:38:55 +0000

    test 3

--- main.rb
+++ main.rb
@@ -1,11 +1,2 @@
-def max(num1, num2, num3)
-  if num1 >= num2 and num1 >= num3
-    return num1
-  elsif num2 >= num1 and num2 >= num3
-    return num2
-  else
-    return num3  
-  end  
-end
-
-puts max(1, 20, 3)
+friends = ["Kevin", "Henry"]
+puts friends[1]
